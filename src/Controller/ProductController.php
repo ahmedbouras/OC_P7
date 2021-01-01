b2d5c963dbba8b7bb73ee1688c49046f6f432fa2
Ahmed <ab.perso@outlook.fr>
2021-01-02 00:44:10 +0100

    doc config for products

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -9,6 +9,9 @@ use FOS\RestBundle\Request\ParamFetcherInterface;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use Symfony\Component\Routing\Annotation\Route;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Nelmio\ApiDocBundle\Annotation\Security;
+use OpenApi\Annotations as OA;
 
 /**
  * @Route("/api", name="api_")
@@ -16,18 +19,29 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProductController extends AbstractFOSRestController
 {
     /**
+     * Get list of all products
      * @Rest\Get("/products", name="list_products")
      * @Rest\QueryParam(
      *      name="page",
      *      requirements="\d+",
-     *      default="1",
-     *      description="The page number."
+     *      default="1"
+     * )
+     * @OA\Parameter(
+     *     name="page",
+     *     in="query",
+     *     description="The page's number.",
+     *     @OA\Schema(type="int")
      * )
      * @Rest\QueryParam(
      *      name="limit",
      *      requirements="\d+",
-     *      default="10",
-     *      description="Limit of items per page."
+     *      default="10"
+     * )
+     * @OA\Parameter(
+     *     name="limit",
+     *     in="query",
+     *     description="Limit of items per page.",
+     *     @OA\Schema(type="int")
      * )
      * @Rest\QueryParam(
      *      name="order",
@@ -35,6 +49,17 @@ class ProductController extends AbstractFOSRestController
      *      default="asc",
      *      description="Sort order (asc or desc)"
      * )
+     * @OA\Response(
+     *     response=200,
+     *     description="Return the products' list of BileMo",
+     *     @Model(type=Product::class)
+     * )
+     * @OA\Response(
+     *     response=401,
+     *     description="JWT Token not found | Invalid JWT Token | Expired JWT Token"
+     * )
+     * @OA\Tag(name="Products")
+     * @Security(name="Bearer")
      * @Rest\View(statusCode=200)
      */
     public function listAction(ParamFetcherInterface $paramFetcher, ProductRepository $productRepository)
@@ -48,11 +73,23 @@ class ProductController extends AbstractFOSRestController
     }
 
     /**
+     * Get details about a product
      * @Rest\Get(
      *      path="/products/{id}",
      *      name="show_product",
      *      requirements={"id"="\d+"}
      * )
+     * @OA\Response(
+     *     response=200,
+     *     description="Return a specific product thanks to its id",
+     *     @Model(type=Product::class)
+     * )
+     * @OA\Response(
+     *     response=401,
+     *     description="JWT Token not found | Invalid JWT Token | Expired JWT Token"
+     * )
+     * @OA\Tag(name="Products")
+     * @Security(name="Bearer")
      * @Rest\View(statusCode=200)
      */
     public function showAction(Product $product)
